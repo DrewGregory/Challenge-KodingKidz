31fa64cd61ddc939a236d7d9440069f0a89614d8
thegreaterfool- <danny.molka@verizon.net>
2015-03-30 22:24:53 -0400

    settings

--- app/src/main/java/com/kodingkidz/alzhelp/AddAlbumActivity.java
+++ app/src/main/java/com/kodingkidz/alzhelp/AddAlbumActivity.java
@@ -1,26 +1,24 @@
 package com.kodingkidz.alzhelp;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class SettingsActivity extends ActionBarActivity {
+public class AddAlbumActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_add_album);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        getMenuInflater().inflate(R.menu.menu_add_album, menu);
         return true;
     }
 
@@ -37,17 +35,5 @@ public class SettingsActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void addPicture (View button)
-    {
-        startActivity(new Intent(this, AddPictureActivity.class));
-    }
-    public void addDescription (View button)
-    {
-        startActivity(new Intent(this, AddDescriptionActivity.class));
-    }
-    public void addAlbum (View button)
-    {
-        startActivity(new Intent(this, AddAlbumActivity.class));
     }
 }
